856fc0e1c08b0ef74fb2cc508031fbcc14a8c0e9
Noé TATOUD <noe@bearstudio.fr>
2025-09-11 11:22:27 +0200

    feat: setup tailwindccs and remove existing styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,13 @@ import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap(), react()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
