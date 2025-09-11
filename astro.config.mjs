6232b870212d0fe5c9417b3239f7411cb1fa0432
Noé TATOUD <noe@bearstudio.fr>
2025-09-11 12:32:36 +0200

    feat: astro config and env hint

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,34 @@
 // @ts-check
 
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 
 import tailwindcss from '@tailwindcss/vite';
 
+// Relative import is required
+import { getSiteUrl } from './src/lib/site/get-site-url';
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: getSiteUrl(),
+  trailingSlash: 'never',
+  redirects: {
+    '/': '/blog',
+  },
+
+  env: {
+    schema: {
+      ENV_NAME: envField.string({
+        context: 'server',
+        access: 'public',
+        optional: true,
+      }),
+    },
+  },
   integrations: [mdx(), sitemap(), react()],
+
   vite: {
     plugins: [tailwindcss()],
   },
--- eslint.config.ts
+++ eslint.config.ts
@@ -4,6 +4,7 @@ import astro from 'eslint-plugin-astro';
 import reactHooks from 'eslint-plugin-react-hooks';
 import unicorn from 'eslint-plugin-unicorn';
 import { defineConfig, globalIgnores } from 'eslint/config';
+import globals from 'globals';
 import tslint from 'typescript-eslint';
 
 export default defineConfig(
@@ -16,7 +17,11 @@ export default defineConfig(
   // For typescript and react files only
   {
     files: ['**/*.ts', '**/*.tsx'],
-
+    languageOptions: {
+      globals: {
+        ...globals.nodeBuiltin,
+      },
+    },
     extends: [
       react.configs['recommended-typescript'],
       reactHooks.configs['recommended-latest'],
