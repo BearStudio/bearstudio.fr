ae8335d78cd413951834e54c0faa60318c720ff3
Noé TATOUD <noe@bearstudio.fr>
2025-09-12 11:12:41 +0200

    feat(blog/schema): Add languages

--- src/schemas/blog.ts
+++ src/schemas/blog.ts
@@ -1,14 +1,13 @@
 import { z, type SchemaContext } from 'astro:content';
 
-import { zu } from '@/lib/zod-utils';
-
 export type Blog = z.infer<ReturnType<typeof zBlog>>;
 export const zBlog = ({ image }: SchemaContext) =>
   z.object({
     title: z.string(),
+    languages: z.array(z.string()),
     excerpt: z.string().optional(),
     heroImage: image().optional(),
-    authors: z.array(zu.todo()).optional(),
+    authors: z.array(z.string()).optional(),
     date: z.date(),
     tags: z.array(z.string()).optional(),
     categories: z.array(z.string()).optional(),
