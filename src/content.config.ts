f2309a7872b8201fb92748d0220d526a371f5895
Gregoire <gregoire@bearstudio.fr>
2025-10-16 17:37:48 +0200

    feat: custom Loader

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,11 +1,42 @@
 import { defineCollection } from 'astro:content';
-import { glob } from 'astro/loaders';
+import { glob, type Loader } from 'astro/loaders';
 
 import { zBlog } from '@/schemas/blog';
 
+const customBlogLoader: Loader = {
+  ...glob,
+  name: 'Page Loader',
+  load: async (loaderParams) => {
+    const { store } = loaderParams;
+    const baseLoader = glob({
+      base: './src/content/blog',
+      pattern: '**/*.{md,mdx}',
+    });
+
+    await baseLoader.load.call(this, loaderParams);
+    console.log('entries');
+    console.log(store.entries());
+
+    let items = [...store.entries()].map(([_, value]) => value);
+    items = items.map((item) => {
+      return {
+        ...item,
+        data: {
+          ...item.data,
+          title: 'Title surchargé !',
+        },
+      };
+    });
+    store.clear();
+    items.forEach((item) => {
+      store.set({ ...item });
+    });
+  },
+};
+
 export const collections = {
   blog: defineCollection({
-    loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
+    loader: customBlogLoader,
     schema: zBlog,
   }),
 };
