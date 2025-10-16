efe3292e0341cac3e3d254b84538ce378142c0c5
Gregoire <gregoire@bearstudio.fr>
2025-10-16 11:36:19 +0200

    feat: linked conferences to specific event

--- src/lib/conferences.ts
+++ src/lib/conferences.ts
@@ -9,6 +9,20 @@ type Params = {
 const isPublished = (post: CollectionEntry<'conferences'>) =>
   post.data.state === 'published' || !import.meta.env.PROD;
 
+type HasSpecificEventProps = {
+  conference: CollectionEntry<'conferences'>;
+  event: CollectionEntry<'events'>;
+};
+
+const hasSpecificEvent = ({ conference, event }: HasSpecificEventProps) => {
+  const selectedPost = (conference.data?.instances ?? []).find(
+    (instance) => instance.event?.id === event.id
+  );
+
+  if (selectedPost) return selectedPost;
+  return;
+};
+
 export async function getConferencesCollection({
   limit = undefined,
 }: Params = {}) {
@@ -26,27 +40,45 @@ type GetEventsFromConferenceParams = {
 
 type InstanceTest = Array<
   Omit<ConferenceInstance, 'event'> & {
-    event?: CollectionEntry<'events'> | undefined;
+    event?: CollectionEntry<'events'>;
   }
 >;
 
-export async function getEventsFromConference({
+export async function getConferenceEvents({
   conference,
 }: GetEventsFromConferenceParams): Promise<InstanceTest> {
   return await Promise.all(
     (conference?.instances ?? []).map(async (instance) => {
-      if (!instance || !instance.event) return instance;
       // eslint-disable-next-line no-unused-vars
       const { event, ...clearInstance } = instance;
 
+      if (!instance || !instance.event) return clearInstance;
+
       const eventEntry = await getEntry(instance.event);
 
-      const test = {
+      return {
         ...clearInstance,
-        event: eventEntry,
+        event: eventEntry as CollectionEntry<'events'>,
       };
-
-      return test;
     })
   );
+}
+
+type GetConferencesLinkedToEventProps = Params & {
+  event: CollectionEntry<'events'>;
+};
+
+export async function getConferencesLinkedToEvent({
+  event,
+  limit = undefined,
+}: GetConferencesLinkedToEventProps) {
+  const posts = (await getCollection('conferences'))
+    .filter((conference) => hasSpecificEvent({ conference, event }))
+    .filter((x) => x);
+
+  if (limit) {
+    return posts.slice(0, limit);
+  }
+
+  return posts;
 }
