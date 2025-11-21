602cc0afe88fb4be51470a1b91c486c977a90e0e
Yoann Fleury <yoann.fleury@yahoo.com>
2025-11-21 16:54:10 +0100

    fix: js to ts

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,17 +1,19 @@
 import { getCollection } from 'astro:content';
 import rss from '@astrojs/rss';
 
+import type { APIRoute } from 'astro';
+
 import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
 
-export async function GET(context) {
+export const GET: APIRoute = async function get(context) {
   const posts = await getCollection('blog');
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
-    site: context.site,
+    site: context.site!,
     items: posts.map((post) => ({
       ...post.data,
       link: `/blog/${post.id}/`,
     })),
   });
-}
+};
