b994c19d6e03951854eb5dc9e173075380bcc5c1
Ivan Dalmet <ivan@dalmet.fr>
2025-12-01 12:17:57 +0100

    wip: polaroids

---
+++ src/components/ui/tilted-card.tsx
@@ -0,0 +1,104 @@
+import { useRef, useState, type ReactNode } from 'react';
+
+import type { SpringOptions } from 'motion/react';
+import { motion, useMotionValue, useSpring } from 'motion/react';
+
+import { useMediaQuery } from '@/hooks/useMediaQuery';
+import { cn } from '@/lib/tailwind/utils';
+
+interface TiltedCardProps {
+  scaleOnHover?: number;
+  rotateAmplitude?: number;
+  children?: ReactNode;
+  className?: string;
+  forceOnMobile?: boolean;
+}
+
+const springValues: SpringOptions = {
+  damping: 30,
+  stiffness: 100,
+  mass: 2,
+};
+
+export default function TiltedCard({
+  scaleOnHover = 1.05,
+  rotateAmplitude = 8,
+  children,
+  className,
+  forceOnMobile,
+}: TiltedCardProps) {
+  const enabled =
+    !useMediaQuery('only screen and (max-width : 640px)') || !!forceOnMobile;
+  const ref = useRef<HTMLDivElement>(null);
+  const x = useMotionValue(0);
+  const y = useMotionValue(0);
+  const rotateX = useSpring(useMotionValue(0), springValues);
+  const rotateY = useSpring(useMotionValue(0), springValues);
+  const scale = useSpring(1, springValues);
+  const opacity = useSpring(0);
+  const rotateFigcaption = useSpring(0, {
+    stiffness: 350,
+    damping: 30,
+    mass: 1,
+  });
+
+  const [lastY, setLastY] = useState(0);
+
+  function handleMouse(e: React.MouseEvent<HTMLElement>) {
+    if (!ref.current) return;
+
+    const rect = ref.current.getBoundingClientRect();
+    const offsetX = e.clientX - rect.left - rect.width / 2;
+    const offsetY = e.clientY - rect.top - rect.height / 2;
+
+    const rotationX = (offsetY / (rect.height / 2)) * -rotateAmplitude;
+    const rotationY = (offsetX / (rect.width / 2)) * rotateAmplitude;
+
+    rotateX.set(rotationX);
+    rotateY.set(rotationY);
+
+    x.set(e.clientX - rect.left);
+    y.set(e.clientY - rect.top);
+
+    const velocityY = offsetY - lastY;
+    rotateFigcaption.set(-velocityY * 0.6);
+    setLastY(offsetY);
+  }
+
+  function handleMouseEnter() {
+    scale.set(scaleOnHover);
+    opacity.set(1);
+  }
+
+  function handleMouseLeave() {
+    opacity.set(0);
+    scale.set(1);
+    rotateX.set(0);
+    rotateY.set(0);
+    rotateFigcaption.set(0);
+  }
+
+  return (
+    <div
+      ref={ref}
+      className={cn(
+        'relative flex flex-col [perspective:800px] hover:z-10',
+        className
+      )}
+      onMouseMove={enabled ? handleMouse : undefined}
+      onMouseEnter={enabled ? handleMouseEnter : undefined}
+      onMouseLeave={enabled ? handleMouseLeave : undefined}
+    >
+      <motion.div
+        className="relative flex flex-1 flex-col [transform-style:preserve-3d]"
+        style={{
+          rotateX,
+          rotateY,
+          scale,
+        }}
+      >
+        {children}
+      </motion.div>
+    </div>
+  );
+}
