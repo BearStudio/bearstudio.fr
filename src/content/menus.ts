3dcc079e55c1197422841b0f9535594c22df8905
Gregoire-Bearstudio <80386314+Gregoire-Bearstudio@users.noreply.github.com>
2025-12-12 15:23:57 +0100

    Feat - hook i18n translations (#20)
    feat - hook i18n translations

--- src/content/menus.ts
+++ src/content/menus.ts
@@ -13,6 +13,7 @@ import {
 } from 'react-icons/pi';
 
 import type { Locales } from '@/i18n';
+import type { translations } from '@/i18n/ui';
 import { ROUTES } from '@/routes.gen';
 
 type MainMenuScope = 'all' | 'mobile' | 'desktop';
@@ -20,6 +21,7 @@ type MainMenuLevel = 'primary' | 'secondary';
 
 export type MenuItem = {
   icon: FC<{ className?: string }>;
+  key?: keyof (typeof translations)['fr'];
   iconActive: FC<{ className?: string }>;
   label: string;
   getHref: (locale: Locales) => string;
@@ -32,6 +34,7 @@ const MAIN_MENU: Array<MenuItem> = [
   {
     label: 'Accueil',
     getHref: (locale) => lunalink(ROUTES[locale].__path, {}),
+    key: 'nav.home',
     exact: true,
     icon: PiHouseDuotone,
     iconActive: PiHouseFill,
@@ -46,6 +49,7 @@ const MAIN_MENU: Array<MenuItem> = [
       }
       return lunalink(ROUTES.fr.prestations.__path, {});
     },
+    key: 'nav.services',
     icon: PiSketchLogoDuotone,
     iconActive: PiSketchLogoFill,
     scope: 'all',
@@ -59,6 +63,7 @@ const MAIN_MENU: Array<MenuItem> = [
       }
       return lunalink(ROUTES.fr.equipe.__path, {});
     },
+    key: 'nav.team',
     icon: PiUsersThreeDuotone,
     iconActive: PiUsersThreeFill,
     scope: 'all',
@@ -67,6 +72,7 @@ const MAIN_MENU: Array<MenuItem> = [
   {
     label: 'Blog',
     getHref: (locale) => lunalink(ROUTES[locale].blog.__path, {}),
+    key: 'nav.blog',
     icon: PiNoteDuotone,
     iconActive: PiNoteFill,
     scope: 'all',
