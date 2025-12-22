70e60664b8af82d1515af22328704b00a2a06efe
Ivan Dalmet <ivan@dalmet.fr>
2025-12-22 17:43:25 +0100

    wip: start home translation

--- src/i18n/en/index.ts
+++ src/i18n/en/index.ts
@@ -1,6 +1,7 @@
 import blog from './blog.json';
 import common from './common.json';
+import home from './home.json';
 import services from './services.json';
 import team from './team.json';
 
-export default { common, blog, services, team } as const;
+export default { common, blog, home, services, team } as const;
