aeef81ef07a03cb5e206391208e88a69e4cdfa95
Ivan Dalmet <ivan@dalmet.fr>
2025-12-23 15:48:35 +0100

    fix: nav primary button

--- src/components/nav/nav-primary-button.tsx
+++ src/components/nav/nav-primary-button.tsx
@@ -1,16 +1,15 @@
-import { lunalink } from '@bearstudio/lunalink';
 import { PiEnvelopeBold } from 'react-icons/pi';
 
+import { getLink } from '@/lib/link';
 import { navButtonVariants } from '@/components/nav/style';
 import type { Locale } from '@/i18n/utils';
 import { getTranslationFn } from '@/i18n/utils';
-import { ROUTES } from '@/routes.gen';
 
 export const NavPrimaryButton = (props: { locale: Locale }) => {
   const t = getTranslationFn(props.locale);
   return (
     <a
-      href={lunalink(ROUTES[props.locale].contact.__path, {})}
+      href={getLink('/fr/contact', props.locale, {})}
       className={navButtonVariants()}
     >
       <PiEnvelopeBold className="opacity-60" />
