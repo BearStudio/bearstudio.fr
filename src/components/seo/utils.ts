1af1485ad835eba652f3261c771ced572dbfb4bf
Yoann Fleury <yoann.fleury@yahoo.com>
2025-12-26 11:32:06 +0100

    feat: add person structured data

--- src/components/seo/utils.ts
+++ src/components/seo/utils.ts
@@ -2,6 +2,7 @@ import type { ExtractParams } from '@bearstudio/lunalink';
 import { fromEntries, map, pipe } from 'remeda';
 
 import { getLink, type DefaultLocaleRoutePaths } from '@/lib/link';
+import { getSiteUrl } from '@/lib/site/get-site-url';
 import { locales } from '@/i18n';
 
 export function getAlternates<P extends DefaultLocaleRoutePaths>(
@@ -13,4 +14,8 @@ export function getAlternates<P extends DefaultLocaleRoutePaths>(
     map((locale) => [locale, getLink(path, locale, params, true)] as const),
     fromEntries()
   );
+}
+
+export function getId(type: 'organization' | 'website') {
+  return `${getSiteUrl()}/#${type}`;
 }
