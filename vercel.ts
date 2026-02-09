e67fdbdb5caab3c7ab1861c5b15ded8054e31b5c
Yoann Fleury <yoann.fleury@yahoo.com>
2026-02-09 16:17:11 +0100

    feat: redirects in ts not json

--- vercel.ts
+++ vercel.ts
@@ -1,7 +1,16 @@
 import { routes, type VercelConfig } from '@vercel/config/v1';
 
+import { articles, categories, members, pages } from './redirects';
+
 export const config: VercelConfig = {
-  redirects: [routes.redirect('/', '/fr', { permanent: true })],
+  redirects: [
+    routes.redirect('/', '/fr', { permanent: true }),
+    ...[...categories, ...pages, ...members, ...articles].map((category) =>
+      routes.redirect(category.source, category.destination, {
+        statusCode: category.statusCode,
+      })
+    ),
+  ],
   headers: [
     routes.header('/(.*)', [{ key: 'X-Robots-Tag', value: 'noindex' }], {
       has: [{ type: 'host', value: 'bearstudio-site-2026.vercel.app' }],
