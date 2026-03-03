a67880a216c68138c379411753b83f9262862e87
Renan Decamps <renan.decamps@gmail.com>
2026-03-03 17:50:27 +0700

    wip

--- src/components/skills/skill-card.tsx
+++ src/components/skills/skill-card.tsx
@@ -16,13 +16,13 @@ export const SkillCard = (props: {
         id: props.skill.data._computed.slug,
       })}
       onDragStart={(e) => e.preventDefault()}
-      className="bg-card border-card-border flex flex-1 flex-col items-center gap-3 overflow-hidden rounded-md border border-b-3 p-6 transition-all hover:-translate-y-1.5 hover:shadow-xl"
+      className="bg-card border-card-border flex flex-1 gap-3 overflow-hidden rounded-md border border-b-3 p-3 transition-all hover:-translate-y-1.5 hover:shadow-xl"
     >
       {!!props.iconImage && (
         <ImgAstroReact
           image={props.iconImage}
           alt={props.skill.data.name}
-          className="size-12"
+          className="size-8"
         />
       )}
       <h2 className="font-heading text-center text-lg font-bold">
