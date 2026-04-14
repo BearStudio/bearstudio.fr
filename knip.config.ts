9bbffc028e99aa0601f29739a570d007ca3c8a34
atama <atama.heitekava@epitech.eu>
2026-04-14 12:12:03 +0200

    [ADD] KINP - workflow - Create the new workflow using knip, and create its command for local run, and config file
    No correction include for the error return by those commands

---
+++ knip.config.ts
@@ -0,0 +1,9 @@
+import type { KnipConfig } from 'knip';
+
+const config: KnipConfig = {
+  entry: [
+    'vercel.ts', // Vercel config entry point (used by Vercel's adapter)
+  ],
+};
+
+export default config;
